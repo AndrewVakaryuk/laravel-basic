3576061c4bc3ee79619bf01b0db945487d724c18
av <av@art-code.team>
2021-09-14 14:08:50 +0300

    insert with model

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -2,7 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Category;
 use Illuminate\Http\Request;
+use Illuminate\Support\Carbon;
+use Illuminate\Support\Facades\Auth;
 
 class CategoryController extends Controller
 {
@@ -19,7 +22,18 @@ class CategoryController extends Controller
         ],
         [
             'category_name.required' => 'Тексту дай!!!',
+            'category_name.unique' => 'Уже було!!!',
             'category_name.max' => 'Забагато тексту!!!',
         ]);
+        Category::insert([
+            'category_name' => $request->category_name,
+            'user_id' => Auth::user()->id,
+            'created_at' => Carbon::now(),
+        ]);
+//        $category = new Category();
+//        $category->category_name = $request->category_name;
+//        $category->user_id = Auth::user()->id;
+//        $category->save();
+        return Redirect()->back()->with('success', 'Категорія: ' . $request->category_name . ' успішно додана');
     }
 }
--- app/Models/Category.php
+++ app/Models/Category.php
@@ -5,6 +5,9 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
+/**
+ * @method static insert(array $array)
+ */
 class Category extends Model
 {
     use SoftDeletes;
