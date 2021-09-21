7b367b7adfb2ae3d3d7f805b7769991a4f9ce296
av <av@art-code.team>
2021-09-21 18:10:08 +0300

    frontend template setup

--- routes/web.php
+++ routes/web.php
@@ -17,11 +17,11 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
 
 Route::get('/home', function () {
-    return view('welcome');
+    return view('home');
 });
 
 Route::get('/about', function () {
